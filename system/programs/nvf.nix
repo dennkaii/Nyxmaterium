1eaaf086f01ff9d7f4d8e860dcc7ad0a4bfe4546
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-03-07 10:53:49 -0500

    nvf: enable lsp and format on save

--- system/programs/nvf.nix
+++ system/programs/nvf.nix
@@ -3,63 +3,67 @@
   pkgs,
   config,
   ...
-}:{
-imports = [
-  inputs.nvf.nixosModules.default
-];
+}: {
+  imports = [
+    inputs.nvf.nixosModules.default
+  ];
   programs.nvf = {
     enable = true;
     settings = {
-        vim = {
+      vim = {
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+        };
         languages = {
-    enableLSP = true;
-    enableFormat = true;
-    enableTreesitter = true;
-    nix.enable = true;
-    rust = {
-      enable = true;
-      # crates.enable = true;
-    };
-    html.enable = true;
-    python.enable = true;
-    markdown.enable = true;
-    bash.enable = true;
-    lua.enable = true;
-  };
-         package = pkgs.neovim-unwrapped;
-      vimAlias = false;
-      viAlias = false;
-
-      autoIndent = true;
-      autocomplete.nvim-cmp.enable = true;
-      autopairs.nvim-autopairs.enable = true;
-      enableEditorconfig = true;
-      preventJunkFiles = true;
-      enableLuaLoader = true;
-      useSystemClipboard = true;
-      spellcheck = {
-        enable = true;
-        languages = ["en"];
-      };
-      hideSearchHighlight = true;
-      searchCase = "smart";
-      splitRight = true;
-    
-          theme = {
+          enableLSP = true;
+          enableFormat = true;
+          enableTreesitter = true;
+          nix.enable = true;
+          rust = {
             enable = true;
-            name = "oxocarbon";
-            style = "dark";
+            # crates.enable = true;
           };
+          html.enable = true;
+          python.enable = true;
+          markdown.enable = true;
+          bash.enable = true;
+          lua.enable = true;
+        };
+        package = pkgs.neovim-unwrapped;
+        vimAlias = false;
+        viAlias = false;
 
-          comments = {
-            comment-nvim.enable = true;
-          };
+        autoIndent = true;
+        autocomplete.nvim-cmp.enable = true;
+        autopairs.nvim-autopairs.enable = true;
+        enableEditorconfig = true;
+        preventJunkFiles = true;
+        enableLuaLoader = true;
+        useSystemClipboard = true;
+        spellcheck = {
+          enable = true;
+          languages = ["en"];
+        };
+        hideSearchHighlight = true;
+        searchCase = "smart";
+        splitRight = true;
 
-          binds = {
-            # whichKey.enable = true;
-            cheatsheet.enable = true;
-          };
+        theme = {
+          enable = true;
+          name = "oxocarbon";
+          style = "dark";
+        };
+
+        comments = {
+          comment-nvim.enable = true;
+        };
+
+        binds = {
+          # whichKey.enable = true;
+          cheatsheet.enable = true;
         };
       };
+    };
   };
 }
