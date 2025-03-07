fe73631a46e107ebc2e89a9338a0ac48699e9b71
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-03-07 13:17:38 +0000

    system/core: initilized, created boot and users respectively

---
+++ system/core/boot.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+{
+  boot = {
+
+  kernelModules = [ "kvm-amd" ];
+  blacklistedKernelModules = ["ucsi_acpi"];
+  kernelPackages = pkgs.linuxPackages_xanmod;
+  extraModulePackages = [ ];
+  supportedFilesystems.ntfs = true;
+
+  initrd = {
+    availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
+    kernelModules = ["amdgpu""asus-wmi""asus-nb-wmi" ];
+    systemd.enable = true;
+		availableKernelModules = ["uas"];
+  };
+
+  loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    grub = {
+     enable = true;
+     efiSupport = true;
+     devices = ["nodev"];
+    };
+  };
+
+  zfs = {
+    devNodes = "/dev/disk/by-partuuid";
+    requestEncryptionCredentials = true;
+  };
+    };
+  
+}
---
+++ system/core/users.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  ...
+}: let
+ cfg = config.users;
+  inherit (lib) mkOption types;
+in {
+   options.users = {
+    main = mkOption {
+      type = with types; str;
+      description = ''
+        Main user
+      '';
+    };
+    host = mkOption {
+      type = with types; str;
+      description = ''
+        Host name
+      '';
+    };
+
+    groups = mkOption {
+      type = with types; listOf str;
+      default = [];
+      description = ''
+        Extra groups the main user will be apart of.
+      '';
+    };
+  };
+  config = {
+  users.users = {
+    root = {
+      isSystemUser = true;
+      group = "root";
+      initialPassword = "123456";
+    };
+    dennkaii = {
+      uid = 1000;
+      isNormalUser = true;
+      group = "users";
+      initialPassword = "123456";
+      extraGroups = ["wheel" "networkmanager"]++cfg.groups;
+      };
+    mutableUsers = false;
+    };
+  };
+}
