f13cb17621c0f92d6d137a366ee76ba85289e227
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-03-08 11:25:36 -0500

    flake:add .envrc

--- flake.nix
+++ flake.nix
@@ -2,38 +2,49 @@
   description = "A very basic flake";
 
   outputs = inputs:
-  inputs.flake-parts.lib.mkFlake{inherit inputs;}{
-  systems = ["x86_64-linux"];
-    imports = [
-    ./hosts
-    ./system
-    ];
-    perSystem = {
-      config,
-      pkgs,
-      ...
-    }:{
-      #deshells go here
-      formatter = pkgs.alejandra;
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [
+        ./hosts
+        ./system
+      ];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        #deshells go here
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.alejandra
+              pkgs.git
+              pkgs.nodePackages.prettier
+            ];
+            name = "dots";
+            DIRENV_LOG_FORMAT = "";
+          };
+        };
+        formatter = pkgs.alejandra;
+      };
     };
-  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     niri-flake = {
-       url="github:Sodiboo/niri-flake";
-     };
-  nvf.url = "github:notashelf/nvf";
-  systems.url = "github:nix-systems/default-linux";
-flake-parts = {
+      url = "github:Sodiboo/niri-flake";
+    };
+    nvf.url = "github:notashelf/nvf";
+    systems.url = "github:nix-systems/default-linux";
+    flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-     flake-utils = {
+    flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
     };
-     zen-browser = {
+    zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
