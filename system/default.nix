3ca6f5a859e0423670a4f833b3e9e8cf16bbdf94
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-03-08 10:57:43 -0500

    system: define laptop

--- system/default.nix
+++ system/default.nix
@@ -1,2 +1,22 @@
-{...}:{
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.laptop;
+  inherit (lib) mkOption types;
+in {
+  options.laptop = mkOption {
+    type = with types; listOf path;
+    default = [];
+    description = ''       
+      Paths that will be added to laptop'';
+  };
+  config = {
+    laptop = [
+      ./core
+      ./core/boot.nix
+      ./programs
+    ];
+  };
 }
