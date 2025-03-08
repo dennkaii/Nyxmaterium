8673bcce4187d5fbbf066907ac28829cdd9c898c
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-03-08 10:32:46 -0500

    system/core:fix typo in users

--- system/core/users.nix
+++ system/core/users.nix
@@ -4,10 +4,10 @@
   config,
   ...
 }: let
- cfg = config.users;
+  cfg = config.users;
   inherit (lib) mkOption types;
 in {
-   options.users = {
+  options.users = {
     egroups = mkOption {
       type = with types; listOf str;
       default = [];
@@ -15,20 +15,30 @@ in {
         Extra groups the main user will be apart of.
       '';
     };
+    epackages = mkOption {
+      type = with types; listOf package;
+      default = [];
+      description = ''
+        packages that will be added to user
+      '';
+    };
   };
   config = {
-  users.users = {
-    root = {
-      isSystemUser = true;
-      group = "root";
-      initialPassword = "123456";
-    };
-    dennkaii = {
-      uid = 1000;
-      isNormalUser = true;
-      group = "users";
-      initialPassword = "123456";
-      extraGroups = ["wheel" "networkmanager"]++cfg.egroups;
+    users.users = {
+      root = {
+        isSystemUser = true;
+        group = "root";
+        initialPassword = "123456";
+      };
+      dennkaii = {
+        uid = 1000;
+        isNormalUser = true;
+        group = "users";
+        initialPassword = "123456";
+        packages =
+          []
+          ++ cfg.epackages;
+        extraGroups = ["wheel" "networkmanager"] ++ cfg.egroups;
       };
     };
     users.mutableUsers = false;
