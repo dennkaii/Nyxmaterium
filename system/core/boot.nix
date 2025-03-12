f6de549f17cf46852d83f2323ad4e3f0b68b4a53
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-03-12 14:47:34 -0400

    hosts,system: typos

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -2,38 +2,37 @@
   pkgs,
   config,
   ...
-}:
-{
-boot = {
+}: {
+  boot = {
+    kernelModules = ["kvm-amd"];
+    kernelParams = ["nvidia-drm.modeset=1"];
+    blacklistedKernelModules = ["ucsi_acpi" "noveau"];
+    kernelPackages = pkgs.linuxPackages_xanmod;
+    extraModulePackages = [];
+    supportedFilesystems.ntfs = true;
 
-  kernelModules = [ "kvm-amd" ];
-  blacklistedKernelModules = ["ucsi_acpi"];
-  kernelPackages = pkgs.linuxPackages_xanmod;
-  extraModulePackages = [ ];
-  supportedFilesystems.ntfs = true;
-
-  initrd = {
-    availableKernelModules = ["uas" "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
-    kernelModules = ["amdgpu""asus-wmi""asus-nb-wmi" ];
-    systemd.enable = true;
-  };
-
-  loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
+    initrd = {
+      availableKernelModules = ["uas" "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
+      kernelModules = ["amdgpu" "asus-wmi" "asus-nb-wmi"];
+      systemd.enable = true;
     };
 
-    grub = {
-     enable = true;
-     efiSupport = true;
-     devices = ["nodev"];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+
+      grub = {
+        enable = true;
+        efiSupport = true;
+        devices = ["nodev"];
+      };
+    };
+
+    zfs = {
+      devNodes = "/dev/disk/by-partuuid";
+      requestEncryptionCredentials = true;
     };
   };
-
-  zfs = {
-    devNodes = "/dev/disk/by-partuuid";
-    requestEncryptionCredentials = true;
-  };
- };  
 }
