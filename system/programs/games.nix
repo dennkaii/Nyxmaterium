12e1b55d324500b413c8c812fb0ccd912208f901
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-04-12 18:31:51 -0400

    system/steam:override steam pkgs with some needed dependencies for games

--- system/programs/games.nix
+++ system/programs/games.nix
@@ -7,21 +7,35 @@
   programs = {
     gamescope = {
       enable = true;
-      capSysNice = true;
-      args = [
-        "--rt"
-        "--expose-wayland"
-      ];
+      # capSysNice = true;
+      # args = [
+      #   "--rt"
+      #   "--expose-wayland"
+      # ];
     };
 
     steam = {
       enable = true;
-
+      package = pkgs.steam.override {
+        extraPkgs = pkgs:
+          with pkgs; [
+            keyutils
+            gamemode
+            libkrb5
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+          ];
+      };
+      # gamescopeSession.enable = true;
       extraCompatPackages = [
         pkgs.proton-ge-bin
       ];
-
-      gamescopeSession.enable = true;
     };
   };
 }
