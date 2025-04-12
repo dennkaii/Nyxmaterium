79fc7d1eca773fe4a588f5acefe18d12a7f4363c
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-04-12 18:30:31 -0400

    wrappers/nyxt:update nyxt to a newer version.
    thanks to https://github.com/shaunsingh/nix-darwin-dotfiles.gita

--- wrappers/nyxt/default.nix
+++ wrappers/nyxt/default.nix
@@ -1,5 +1,47 @@
-{pkgs, ...}: {
-  wrappers.nyxt = {
-    basePackage = pkgs.nyxt;
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  # enable DRM support on webkit
+  webkitgtk-eme = pkgs.webkitgtk_4_1.overrideAttrs (oldAttrs: rec {
+    buildInputs =
+      oldAttrs.buildInputs
+      ++ [
+        pkgs.libgpg-error
+      ];
+    cmakeFlags =
+      oldAttrs.cmakeFlags
+      ++ [
+        "-DENABLE_ENCRYPTED_MEDIA=ON"
+      ];
+  });
+in {
+  config = {
+    wrappers.nyxt = {
+      basePackage = pkgs.nyxt.overrideAttrs (oldAttrs: rec {
+        pname = "nyxt4-3";
+        version = "4.0.0-pre-release-3";
+        src = pkgs.fetchzip {
+          url = "https://github.com/atlas-engineer/nyxt/releases/download/${version}/nyxt-${version}-source-with-submodules.tar.xz";
+          hash = "sha256-T5p3OaWp28rny81ggdE9iXffmuh6wt6XSuteTOT8FLI=";
+          stripRoot = false;
+        };
+        LD_LIBRARY_PATH =
+          lib.makeLibraryPath
+          [
+            pkgs.glib
+            pkgs.gobject-introspection
+            pkgs.gdk-pixbuf
+            pkgs.cairo
+            pkgs.pango
+            pkgs.gtk3
+            pkgs.openssl
+            pkgs.libfixposix
+            webkitgtk-eme
+          ];
+      });
+    };
   };
 }
