270dde8a68505ead704ea8cbec79f4161c16c3da
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-04-20 12:21:25 -0400

    home/ghostty: add some configurations

--- home/terminal/emulators/ghostty.nix
+++ home/terminal/emulators/ghostty.nix
@@ -1,5 +1,59 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   programs.ghostty = {
     enable = true;
+    package = inputs.ghostty.packages.${pkgs.system}.default;
+    settings = {
+      confirm-close-surface = false;
+      window-decoration = false;
+      background-opacity = 0.8;
+      keybind = [
+        "ctrl+t=new_tab"
+        "ctrl+w=close_tab"
+        "ctrl+right=next_tab"
+        "ctrl+left=previous_tab"
+        "ctrl+one=goto_tab:1"
+        "ctrl+two=goto_tab:2"
+        "ctrl+three=goto_tab:3"
+        "ctrl+four=goto_tab:4"
+        "ctrl+five=goto_tab:5"
+        "ctrl+six=goto_tab:6"
+        "ctrl+seven=goto_tab:7"
+        "ctrl+eight=goto_tab:8"
+
+        "alt+r=new_split:right"
+        "alt+d=new_split:down"
+        "alt+w=close_surface"
+        "alt+shift+up=goto_split:up"
+        "alt+shift+down=goto_split:down"
+        "alt+shift+left=goto_split:left"
+        "alt+shift+right=goto_split:right"
+
+        "alt+left=esc:b"
+        "alt+right=esc:f"
+
+        "ctrl+shift+t=unbind"
+        "ctrl+shift+w=unbind"
+        "ctrl+shift+right=unbind"
+        "ctrl+shift+left=unbind"
+        "alt+one=unbind"
+        "alt+two=unbind"
+        "alt+three=unbind"
+        "alt+four=unbind"
+        "alt+five=unbind"
+        "alt+six=unbind"
+        "alt+seven=unbind"
+        "alt+eight=unbind"
+        "ctrl+shift+o=unbind"
+        "ctrl+shift+e=unbind"
+        "ctrl+alt+up=unbind"
+        "ctrl+alt+down=unbind"
+        "ctrl+alt+left=unbind"
+        "ctrl+alt+right=unbind"
+      ];
+    };
   };
 }
