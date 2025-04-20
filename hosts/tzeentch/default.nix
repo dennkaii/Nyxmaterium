33331c421028f275227e0360f35c356cc64a122a
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-04-19 20:51:36 -0400

    hosts/tzeentch: tlp -> auto-cpufreq

--- hosts/tzeentch/default.nix
+++ hosts/tzeentch/default.nix
@@ -14,5 +14,17 @@
 
   services.asusd.enable = true;
   services.supergfxd.enable = true;
-  services.tlp.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
 }
