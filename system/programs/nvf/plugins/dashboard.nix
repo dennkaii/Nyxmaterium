0df9dbbb5da8b458a7263f42dc8f78b855fb368e
dennkaii <70287696+dennkaii@users.noreply.github.com>
2025-05-02 23:50:13 -0400

    system/nvf: alpha -> startify

--- system/programs/nvf/plugins/dashboard.nix
+++ system/programs/nvf/plugins/dashboard.nix
@@ -1,7 +1,45 @@
 {
   programs.nvf.settings.vim = {
-    dashboard.alpha = {
+    dashboard.startify = {
       enable = true;
+      bookmarks = [
+        {
+          nyxmaterium = "~/projects/Nyxmaterium";
+        }
+      ];
+      lists = [
+        {
+          header = [
+            "Bookmarks"
+          ];
+          type = "bookmarks";
+        }
+        {
+          header = [
+            "MRU"
+          ];
+          type = "files";
+        }
+        {
+          header = [
+            "MRU Current Directory"
+          ];
+          type = "dir";
+        }
+        {
+          header = [
+            "Sessions"
+          ];
+          type = "sessions";
+        }
+
+        {
+          header = [
+            "Commands"
+          ];
+          type = "commands";
+        }
+      ];
     };
   };
 }
