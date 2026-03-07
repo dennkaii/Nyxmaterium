b3bb3f7a87a21fa25c4f4804dbfd23e79d74818d
dennkaii <70287696+dennkaii@users.noreply.github.com>
2026-03-07 11:13:06 -0400

    system/core: nvida bs to make s2idle work(not working yet)

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -8,11 +8,13 @@
     kernelModules = ["kvm-amd"];
     kernelParams = lib.mkAfter [
       "nvidia-drm.modeset=1"
-      "mem_sleep_default=s2idle"
       "no_console_suspend"
       "amd_pstate=active"
       "nvme.noacpi=1"
     ];
+    extraModprobeConfig = ''
+      options nvidia NVreg_TemporaryFilePath=/var/tmp
+    '';
 
     blacklistedKernelModules = ["ucsi_acpi"];
     kernelPackages = pkgs.linuxPackages_xanmod;
@@ -21,12 +23,13 @@
 
     initrd = {
       availableKernelModules = ["uas" "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
-      kernelModules = ["amdgpu" "asus-wmi" "asus-nb-wmi"];
+      kernelModules = ["amdgpu" "asus-wmi" "asus-nb-wmi" "nvidia_uvm" "nvidia_modeset" "nvidia_drm" "nvidia"]; # all nvidia lines are testing
       systemd = {
         enable = true;
-        managerEnvironment = {
-          SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK = "1";
-        };
+        # managerEnvironment = {
+        #   SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK = "1";
+        # };
+        #                       nvidia
       };
     };
 
